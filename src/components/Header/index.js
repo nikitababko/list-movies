62d2cdcc62debada8091a43b3d2db8cf1781dd4f
Nikita Babko <niki.babko@gmail.com>
2021-06-22 18:42:56 +0700

    Added active class for header nav menu.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import SearchIcon from './assets/SearchIcon';
 import FavIcon from './assets/FavIcon';
-import TrendingIcon from './assets/TrendingIcon';
-import InformationIcon from './assets/InformationIcon';
 
 import './index.scss';
 
 const Header = () => {
+  const { pathname } = useLocation();
+
   return (
     <header className="header">
       <nav className="nav">
-        <Link className="nav__link" to="/">
+        <Link
+          className={pathname === '/' ? 'nav__link active' : 'nav__link'}
+          to="/"
+        >
           <SearchIcon />
         </Link>
 
-        <Link className="nav__link" to="/favorites">
+        <Link
+          className={
+            pathname === '/favorites' ? 'nav__link active' : 'nav__link'
+          }
+          to="/favorites"
+        >
           <FavIcon />
         </Link>
-
-        {/* <Link className="nav__link" to="/trendings">
-          <TrendingIcon />
-        </Link>
-
-        <Link className="nav__link" to="/">
-          <InformationIcon />
-        </Link> */}
       </nav>
     </header>
   );
